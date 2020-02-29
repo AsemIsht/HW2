c06ad7810710198ae0199f08e11335c9899a143b
Asem <Asem@DESKTOP-1I0NA2K>
2020-02-29 20:38:45 +0200

    first commit

---
+++ src/testing_HW2/Calculator.java
@@ -0,0 +1,16 @@
+package testing_HW2;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int add(int x, int y) {
+		
+		return x+y;
+		
+	}
+
+}
